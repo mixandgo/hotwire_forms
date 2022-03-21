0afc088eff86d198577a862f606cd801b0a3c4e1
Cezar Halmagean <cezar@mixandgo.com>
2022-03-21 09:42:39 +0200

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+[
+  { name: "John", email: "jdoe@example.com", age: 21 },
+  { name: "Emma", email: "emma@example.com", age: 30 },
+  { name: "Don", email: "don@example.com", age: 34 },
+  { name: "Boris", email: "boris@example.com", age: 62 },
+  { name: "Mary", email: "mary@example.com", age: 18 },
+  { name: "Dave", email: "dave@example.com", age: 29 },
+  { name: "Chris", email: "chris@example.com", age: 51 },
+  { name: "Ed", email: "ed@example.com", age: 43 },
+  { name: "Kevin", email: "kevin@example.com", age: 81 },
+  { name: "Dave", email: "dave@example.com", age: 76 }
+].each(&Contact.method(:create))
